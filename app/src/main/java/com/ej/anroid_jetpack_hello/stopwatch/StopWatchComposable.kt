631b19572e8407cd75ee5c9def1ad47312a851b9
linexy <linexy@naver.com>
2023-11-14 18:00:26 +0900

    Feat: StopWatch Ui 작성

---
+++ app/src/main/java/com/ej/anroid_jetpack_hello/stopwatch/StopWatchComposable.kt
@@ -0,0 +1,14 @@
+package com.ej.anroid_jetpack_hello.stopwatch
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun StopWatchScreen(
+    sec: Int,
+    milli: Int,
+    isRunning: Boolean,
+    lapTimes : List<String>,
+) {
+
+}
