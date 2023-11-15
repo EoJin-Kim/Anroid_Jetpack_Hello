8094d4d4e692a9eb0ae99bac4467259654465d48
linexy <linexy@naver.com>
2023-11-15 10:00:45 +0900

    Feat: StopWatch 완료

--- app/src/main/java/com/ej/anroid_jetpack_hello/MainActivity.kt
+++ app/src/main/java/com/ej/anroid_jetpack_hello/MainActivity.kt
@@ -3,12 +3,33 @@ package com.ej.anroid_jetpack_hello
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ej.anroid_jetpack_hello.stopwatch.StopWatchScreen
+import com.ej.anroid_jetpack_hello.stopwatch.StopWatchViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<StopWatchViewModel>()
 
+            val sec = viewModel.sec.value
+            val milli = viewModel.milli.value
+            val isRunning = viewModel.isRunning.value
+            val lapTimes = viewModel.lapTimes.value
+
+            StopWatchScreen(sec = sec,
+                milli = milli,
+                isRunning = isRunning,
+                lapTimes = lapTimes,
+                onRest = { viewModel.reset() },
+                onToggle = {
+                    if(it) viewModel.pause() else viewModel.start()
+                },
+                onLapTime = {
+                    viewModel.recordLapTime()
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/ej/anroid_jetpack_hello/stopwatch/StopWatchComposable.kt
+++ app/src/main/java/com/ej/anroid_jetpack_hello/stopwatch/StopWatchComposable.kt
@@ -1,6 +1,18 @@
 package com.ej.anroid_jetpack_hello.stopwatch
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ej.anroid_jetpack_hello.R
 
 
 @Composable
@@ -9,6 +21,73 @@ fun StopWatchScreen(
     milli: Int,
     isRunning: Boolean,
     lapTimes : List<String>,
+    onRest: () -> Unit,
+    onToggle : (Boolean) -> Unit,
+    onLapTime : () -> Unit,
 ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "StopWatch")}
+            )
+        }
+    ) {
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+            Row(
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Text(text = "$sec", fontSize = 100.sp)
+                Text(text = "$milli", fontSize = 10.sp)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
 
+            Column(modifier = Modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+            ) {
+                for (lapTime in lapTimes) {
+                    Text(text = lapTime)
+                }
+            }
+            Row(modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                FloatingActionButton(
+                    onClick = {
+                        onRest()
+                    },
+                    backgroundColor = Color.Red
+                ) {
+                    Image(painter = painterResource(id = R.drawable.ic_baseline_refresh_24), contentDescription = "reset")
+                }
+
+                FloatingActionButton(
+                    onClick = {
+                        onToggle(isRunning)
+                    },
+                    backgroundColor = Color.Green
+                ) {
+                    Image(
+                        painter = painterResource(id = if (isRunning) R.drawable.ic_baseline_pause_24 else R.drawable.ic_baseline_play_arrow_24),
+                        contentDescription = "reset/pause"
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        onLapTime()
+                    },
+                ) {
+                    Text(text = "랩 타임")
+                }
+            }
+        }
+
+    }
 }
